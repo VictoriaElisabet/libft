a33de6aa2d8b3e19bc8b7971537b629c05201fbd
vgrankul <vgrankul@student.hive.fi>
2019-10-24 14:20:49 +0300

    Added 9 more functions

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,27 +6,13 @@
 /*   By: vgrankul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 10:26:57 by vgrankul          #+#    #+#             */
-/*   Updated: 2019/10/21 13:47:22 by vgrankul         ###   ########.fr       */
+/*   Updated: 2019/10/24 13:11:23 by vgrankul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-char	*ft_strcpy(char *dst, const char *src)
-{
-	char *ptr;
 
-	ptr = dst;
-	while (*src != '\0')
-	{
-		*dst = *src;
-		dst++;
-		src++;
-	}
-	*dst = '\0';
-	return (ptr);
-}
-int	ft_strlen(char *str)
+int		ft_strlen(char *str)
 {
 	int counter;
 
@@ -38,48 +24,27 @@ int	ft_strlen(char *str)
 	}
 	return (counter);
 }
+
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	count;
-	int		i;
+	size_t	i;
+	size_t	strlen;
 
-	count = 0;
 	i = 0;
-	if (dstsize == 0)
-	{
-		return(ft_strlen(dst))// add the length of the source;
-	}
-	while(*dst != '\0')
+	strlen = ft_strlen((char*)src);
+	while (*dst != '\0' && dstsize > 0)
 	{
 		dst++;
-		count++;
+		i++;
+		dstsize--;
 	}
-//	dstsize = dstsize - count - 1;
-	while((count + 1) < dstsize && *src != '\0')
+	while (dstsize > 1 && *src != '\0')
 	{
 		*dst = *src;
-		src++;
 		dst++;
-		count++;
+		src++;
 	}
-	*dst = '\0';
-	return (count + 1);
-}
-int	main(void)
-{
-	//char test[9];
-	char s1[5] = "ja";
-	char s2[6] = "jovars";
-	//int len;
-
-	//ft_strcpy(test, s1);
-	//len = strlcpy(test, s1, sizeof(test));
-	//printf("%zu", ft_strlcat(test, s2, len));
-	printf("%zu", ft_strlcat(s1, s2, 5));
-	printf("\n%s\n", s1);
-	//printf("%zu", strlcat(test, s2, sizeof(test)));
-	//printf("%d", len);
-	//ft_strlcat(test, s2, len);
-	//printf("%d", len);
-	//printf("%s", test);
+	if (dstsize == 1 || dstsize == 0)
+		*dst = '\0';
+	return (strlen + i);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vgrankul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 10:24:05 by vgrankul          #+#    #+#             */
-/*   Updated: 2019/10/23 17:10:00 by vgrankul         ###   ########.fr       */
+/*   Updated: 2019/10/24 14:08:47 by vgrankul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	ft_strclr(char *s);
 
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
@@ -42,5 +43,14 @@ int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 #endif
